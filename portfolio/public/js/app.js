d50ad86d14a73ecc36330f802a544e60cf9fd283
Timothy Balilo <ts.balilo@gmail.com>
2022-05-03 09:46:43 -0700

    added some variables

--- portfolio/public/js/app.js
+++ portfolio/public/js/app.js
@@ -1,7 +1,20 @@
 const contactForm = document.querySelector('.contact-form');
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let subject = document.getElementById("subject");
+let message = document.getElementById("message");
 
 
 contactForm.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log("button clicked")
+
+
+        let formData = {
+            name: name.value,
+            email: email.value,
+            subject: subject.value,
+            message: message.value
+        }
+        console.log(formData)
 })
